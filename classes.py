0b14d1bc6b4b796a4ac38fe93d94d5500aed6668
AngelesT85 <brawl-ford123456@mail.ru>
2024-10-24 19:55:06 +0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -19,19 +19,15 @@ class Snake:
 
     def __init__(self, field, coords, position):
         '''
-        function create new segment of snake (at start of the game and when eat apple)
-        field (matrix) - field on which snake is crawl.
-        coords (tuple) - where on the field will appear segment of snake. (string, column)
-        position (tuple) - what kind of piece of snake (head/body/tail) and degree for the picture. (head/body/tail, 90)
+function create new segment of snake (at start of the game and when eat apple)
+field (matrix) - field on which snake is crawl.
+coords (tuple) - where on the field will appear segment of snake. (string, column)
+position (tuple) - what kind of piece of snake (head/body/tail) and degree for the picture. (head/body/tail, 90)
         '''
         self.status = position[0]
         self.degree = position[1]
         self.coords = coords
-        
+
         string = coords[0]
         col = coords[1]
         field[string][col] = self
-
-field = Field()
-field.field[0][0] = Snake()
-field.PrintField()
