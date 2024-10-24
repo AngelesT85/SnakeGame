4e53f83004c42d9bd975bc851f2f1b7c93c6d17e
AngelesT85 <brawl-ford123456@mail.ru>
2024-10-24 20:01:08 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -4,5 +4,10 @@ def CreateSnake(field):
     '''
     function create start snake on the (7, 8) (8, 8) (9, 8) coords
     '''
-    for status, row in ("head", "body", "tail"), range(3):
-        snake = Snake(field, (7 + row, 8), (status, 0))
+    d = {
+        0: "head",
+        1: "body",
+        2: "tail"
+    }
+    for row in d:
+        snake = Snake(field, (6 + row, 7), (d[row], 0))
