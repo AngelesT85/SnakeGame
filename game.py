0af8c0fc35c7158cf78466f165af5e8e8190cefb
AngelesT85 <brawl-ford123456@mail.ru>
2024-10-24 19:31:23 +0300

    Create game.py

---
+++ game.py
@@ -0,0 +1,24 @@
+# automaticly pip install pygame
+try:
+    import pygame as pg
+except:
+    from subprocess import check_call
+    from sys import executable
+    check_call([executable, "-m", "pip", "install", "pygame"])
+
+def Snake():
+    pg.init()
+
+    screen = pg.display.set_mode((912, 912))
+    screen.fill((255, 255, 255))
+    
+    is_game = True
+
+    while is_game:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                is_game = False
+        
+        pg.display.flip()
+
+Snake()
