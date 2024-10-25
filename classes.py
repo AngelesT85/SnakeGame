068283d14149edd34b0fd21c9799ba0afd69ced0
AngelesT85 <brawl-ford123456@mail.ru>
2024-10-25 10:20:31 +0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -2,12 +2,19 @@ class Field:
     def __init__(self):
         self.field = [["-" for i in range(15)] for j in range(15)]
     
+    def UpdateField(self):
+        for segment in Snake.Segments:
+            string = segment.coords[0]
+            col = segment.coords[1]
+            self.field[string][col] = segment
+
+    
     def Print(self, change_snake=True):
         '''
-function print field
-change_snake - will objects of Snake print like 'class.__Snake__...' (change_snake = False)
-                                        or
-                objects of Snake will printed like 'o' (change_snake = True (by default))
+        function print field
+        change_snake - will objects of Snake print like 'class.__Snake__...' (change_snake = False)
+                                                or
+                        objects of Snake will printed like 'o' (change_snake = True (by default))
         '''
         for i in self.field:
             for j in i:
@@ -26,17 +33,15 @@ change_snake - will objects of Snake print like 'class.__Snake__...' (change_sna
 class Snake:
     Segments = list()
 
-    def __init__(self, field, coords, position):
+    def __init__(self, coords, position):
         '''
-function create new segment of snake (at start of the game and when eat apple)
-field (matrix) - field on which snake is crawl.
-coords (tuple) - where on the field will appear segment of snake. (string, column)
-position (tuple) - what kind of piece of snake (head/body/tail) and degree for the picture. (head/body/tail, 90)
+        function create new segment of snake (at start of the game and when eat apple)
+        coords (tuple) - where on the field will appear segment of snake. (string, column)
+        position (tuple) - what kind of piece of snake (head/body/tail) and degree for the picture. ('head'/'body'/'tail', 90)
         '''
         self.status = position[0]
         self.degree = position[1]
         self.coords = coords
 
-        string = coords[0]
-        col = coords[1]
-        field[string][col] = self
+        Snake.Segments.append(self)
+    
