7b5209e2cd6616591eccd937c26b6bec6def8f3e
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-10-25 17:57:43 +0300

    Update images.py

--- loads/images.py
+++ loads/images.py
@@ -8,5 +8,10 @@ Pear = Load("pear")
 SnakeTail = Load("snake end")
 SnakeBody = Load("snake middle")
 SnakeHead = Load("snake head")
+<<<<<<< HEAD
 Apple = Load("apple")
 Start = Load("start")
+=======
+SnakeTurn = Load("body turn")
+Apple = Load("apple")
+>>>>>>> 78f36b8cb19ce09bd5f8b2c10c40b7ebbb199835
