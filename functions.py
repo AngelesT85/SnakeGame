dd4cc0f3fcf1eb2f7e477fa5c1c84401fac84b03
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-10-25 17:25:27 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -13,7 +13,7 @@ def CreateSnake():
     for row in d:
         snake = Snake((6 + row, 7), (d[row], 0))
 
-def draw_snake(screen):
+def DrawSnake(screen, field):
     for part_snake in Snake.Segments:
         if part_snake.status == "head":
             screen.blit(SnakeHead, (96 + 48 * part_snake.coords[1], 160 + 48 * part_snake.coords[0]))
@@ -21,3 +21,12 @@ def draw_snake(screen):
             screen.blit(SnakeBody, (96 + 48 * part_snake.coords[1], 160 + 48 * part_snake.coords[0]))
         elif part_snake.status == "tail":
             screen.blit(SnakeTail, (96 + 48 * part_snake.coords[1], 160 + 48 * part_snake.coords[0]))
+
+def DrawFood(screen, field):
+    for i in range(15):
+        for j in range(15):
+            if type(field[i][j]) == Food:
+                screen.blit(field[i][j].image, (96 + 48 * j, 160 + 48 * i))
+
+
+    
