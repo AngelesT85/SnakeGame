1ef4b6c8a94c2db438989db0f3f346230f10fb41
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-10-25 18:11:36 +0300

    Update game.py

--- game.py
+++ game.py
@@ -31,11 +31,17 @@ def Snake():
     while is_game:
         DrawSnake(screen, field.field)
         DrawFood(screen, field.field)
-        food = Food()
-        food.Spawn(field.field)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 is_game = False
+            
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                x, y = pg.mouse.get_pos()
+                if (217 <= x <= 696) and (0 <= y <= 123):
+                    food = Food()
+                    food.Spawn(field.field)
+                    print(1)
+
         
         pg.display.flip()
 
