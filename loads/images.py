a32f496e09d5f0d68418f2841c268ed1829d1d33
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-10-25 17:59:13 +0300

    Update images.py

--- loads/images.py
+++ loads/images.py
@@ -4,14 +4,13 @@ def Load(image):
     return load(f"images/{image}.png")
 
 FieldImg = Load("field")
+
+Apple = Load("apple")
 Pear = Load("pear")
+
 SnakeTail = Load("snake end")
 SnakeBody = Load("snake middle")
 SnakeHead = Load("snake head")
-<<<<<<< HEAD
-Apple = Load("apple")
-Start = Load("start")
-=======
 SnakeTurn = Load("body turn")
-Apple = Load("apple")
->>>>>>> 78f36b8cb19ce09bd5f8b2c10c40b7ebbb199835
+
+Start = Load("start")
