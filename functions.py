08e66cb0065b6cf31dd7c3f9e6641333ef7cc69e
AngelesT85 <brawl-ford123456@mail.ru>
2024-10-25 18:40:19 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -6,12 +6,12 @@ def CreateSnake():
     function create start snake on the (7, 8) (8, 8) (9, 8) coords
     '''
     d = {
-        0: "head",
+        2: "head",
         1: "body",
-        2: "tail"
+        0: "tail"
     }
     for row in d:
-        snake = Snake((6 + row, 7), (d[row], 0, "up"))
+        snake = Snake([6 + row, 7], [d[row], 0, "up"])
 
 def DrawSnake(screen, field):
     for part_snake in Snake.Segments:
