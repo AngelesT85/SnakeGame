8a933002663e08db24229261d434543c37b4537d
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-10-27 18:14:59 +0300

    Update game.py

--- game.py
+++ game.py
@@ -18,18 +18,18 @@ def snake():
     CreateSnake()
     field.UpdateField()
 
-    field.Print()
+    #field.Print()
     
-    result = Snake.Move(field.field, "up")
+    #result = Snake.Move(field.field, "up")
     
-    field.UpdateField()
+    #field.UpdateField()
 
-    for segment in Snake.Segments:
-        print(segment.position[0])
+    #for segment in Snake.Segments:
+    #    print(segment.position[0])
 
-    print(Snake.Segments[1].position)
-    print(result)
-    field.Print()
+    #print(Snake.Segments[1].position)
+    #print(result)
+    #field.Print()
 
     screen = pg.display.set_mode((912, 912))
     screen.fill((255, 255, 255))
@@ -37,24 +37,50 @@ def snake():
     
     is_game = True
     game_start = False
+    lose = False
+    count = 0
 
-    DrawSnake(screen, field.field, SnakeHead, SnakeBody, SnakeTurn, SnakeTail)
 
     while is_game:
+        if count > 15:
+            print(count)
+
+        screen.blit(FieldImg, (0, 0))
         if game_start:
-            Food.Spawn(field.field)
             screen.blit(Restart, (0, 0))
+
+            count += 1
+            if count == 15:
+                count = 0
+                #need doing code about going the snake
+                end_move = Snake.Move(field.field, "up")
+                if not end_move[0]:
+                    lose = True
+
+            if Snake.Length == 225:
+                print("need doing code about winning")
+            
+            elif lose:
+                print("need doing code about defeat")
+
+
+            elif Snake.Number_food <= 100:
+                chance = randint(1, 1000)
+
+                if chance in (250, 500, 750):
+                    Food.Spawn(field.field)
+                    Snake.Number_food += 1
         else:
             screen.blit(Start, (0, 0))
-
-        DrawSnake(screen, field.field, SnakeHead, SnakeBody, SnakeTurn, SnakeTail)
+        DrawSnake(screen)
         DrawFood(screen, field.field)
 
+            
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 is_game = False
             
-            elif event.type == pg.MOUSEBUTTONDOWN:
+            if event.type == pg.MOUSEBUTTONDOWN:
                 x, y = pg.mouse.get_pos()
                 if (217 <= x <= 696) and (0 <= y <= 123):
                     if game_start == False:
@@ -64,8 +90,39 @@ def snake():
 
                         screen.blit(FieldImg, (0, 0))
                         field = Field()
-                        CreateSnake()
                         field.UpdateField()
+                        CreateSnake()
+                        DrawSnake(screen)
+            
+            elif event.type == pg.KEYDOWN:
+                if game_start:
+                    if event.key == pg.K_r:
+                        game_start = False
+                else:
+                    if event.key == pg.K_g:
+                        game_start = True
+
+            if game_start and not lose:
+                if event.type == pg.KEYDOWN:
+                    if event.key in (pg.K_LEFT, pg.K_a):
+                        end_move = Snake.Move(field.field, "left")
+                        if not end_move[0]:
+                            lose = True
+
+                    elif event.key in (pg.K_RIGHT, pg.K_d):
+                        end_move = Snake.Move(field.field, "right")
+                        if not end_move[0]:
+                            lose = True
+
+                    elif event.key in (pg.K_UP, pg.K_w):
+                        end_move = Snake.Move(field.field, "up")
+                        if not end_move[0]:
+                            lose = True
+
+                    elif event.key in (pg.K_DOWN, pg.K_s):
+                        end_move = Snake.Move(field.field, "down")
+                        if not end_move[0]:
+                            lose = True
         
         pg.display.flip()
 
