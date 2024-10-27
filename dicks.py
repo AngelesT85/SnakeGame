8ff295ad2e64618f9e881b916cebd9d67fb618d7
AngelesT85 <brawl-ford123456@mail.ru>
2024-10-27 15:43:06 +0300

    Update dicks.py

--- dicks.py
+++ dicks.py
@@ -1,6 +1,13 @@
 Dires = {
-    "up": 0,
-    "left": 90,
-    "down": 180,
-    "right": 270
+    "up": (0, (-1, 0)),
+    "left": (90, (0, -1)),
+    "down": (180, (1, 0)),
+    "right": (270, (0, 1))
+}
+
+Antonims = {
+    "up": "down",
+    "down": "up",
+    "left": "right",
+    "right": "left"
 }
