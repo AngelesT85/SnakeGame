d56c228c6fd77e3628850697f00194ffd296d414
AngelesT85 <brawl-ford123456@mail.ru>
2024-10-27 09:48:37 +0300

    Update check.py

--- check.py
+++ check.py
@@ -2,14 +2,21 @@ from functions import *
 
 field = Field()
 
-food = Food()
-food.Spawn(field.field)
+Food.Spawn(field.field)
 
 CreateSnake()
 field.UpdateField()
 
-result = Snake.Move(field.field, (1, 0), False, "d")
+print(Snake.Segments)
+
+result = Snake.Move(field.field, (-1, 0), False, "d")
+result = Snake.Move(field.field, (0, 1), True, "right")
+# result = Snake.Move(field.field, (-1, 0), False, "d")
 field.UpdateField()
 
+for segment in Snake.Segments:
+    print(segment.position[0])
+
+print(Snake.Segments)
 print(result)
 field.Print()
