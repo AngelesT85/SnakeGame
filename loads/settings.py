76f55752304172a0d30e1e18d5df10b6d70436ee
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-11-05 12:21:06 +0300

    Update settings.py

--- loads/settings.py
+++ loads/settings.py
@@ -7,8 +7,3 @@ with open("settings.json", "r", encoding="utf-8") as settings:
     if PrintConsoleField:
         SnakeLike_o = settings["Snake Like 'o' "]
         FoodLike_f = settings["Food Like 'f' "]
-<<<<<<< HEAD
-
-    #print(PrintConsoleField, SnakeLike_o, FoodLike_f)
-=======
->>>>>>> 5877d8b1d65de8b3b13bd536017004359b4f2bd9
