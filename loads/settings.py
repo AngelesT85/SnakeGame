1a24ed71e5631b286199843896f31797f239bfd8
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-11-11 18:32:06 +0300

    Update settings.py

--- loads/settings.py
+++ loads/settings.py
@@ -3,6 +3,7 @@ import json
 with open("settings.json", "r", encoding="utf-8") as settings:
     settings = json.load(settings)
     PrintConsoleField = settings["Print Console Field"]
+    SpeedModifier = settings["Speed Modifier"]
     
     if PrintConsoleField:
         SnakeLike_o = settings["Snake Like 'o' "]
